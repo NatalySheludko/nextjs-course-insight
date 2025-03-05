b4cee462046783b0ddb0f4327a80fbd1f6788337
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-06 01:34:45 +0200

    create project structure

---
+++ app/sitemap.ts
@@ -0,0 +1,50 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = 'http://localhost:3000';
+
+    return [
+        {
+            url: `${baseUrl}/`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/about-us`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/services`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/portfolio`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/blog`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/pages`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+        {
+            url: `${baseUrl}/contact-us`,
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 1,
+        },
+    ];
+}
---
+++ types/common.ts
@@ -0,0 +1,36 @@
+// import { StaticImageData } from 'next/image';
+
+// export type TariffType = 'day' | 'night';
+
+// export type CalculationType = 'boiler' | 'light' | 'wm' | 'mw';
+
+// export type Boiler = {
+//     name: string;
+//     price: number;
+//     link: string;
+//     image: StaticImageData;
+//     efficiency: number;
+//     volume: number;
+// };
+
+// export type Advice = {
+//     label: string;
+//     options: string;
+// };
+
+// export interface UnifiedStoreState {
+//     location: string;
+//     tariffType: TariffType;
+//     setLocation: (location: string) => void;
+//     setTariffType: (type: TariffType) => void;
+//     boiler: Boiler | null;
+//     setBoiler: (boiler: Boiler) => void;
+//     advices: { [key in CalculationType]: Advice[] };
+//     setAdvices: (type: CalculationType, newAdvices: Advice[]) => void;
+//     isCalculationDone: boolean;
+//     setCalculationDone: (done: boolean) => void;
+//     calculationType: CalculationType;
+//     setCalculationType: (type: CalculationType) => void;
+//     activeTab: CalculationType;
+//     setActiveTab: (tab: CalculationType) => void;
+// }
