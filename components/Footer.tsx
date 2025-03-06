2774c24eb8d34d00c99d60bb48ea93f12f007f41
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-07 00:45:07 +0200

    complete footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,6 +6,8 @@ import Link from 'next/link';
 
 import SquareLogo from '../public/logoImages/squareLogo.png';
 import CircleLogo from '../public/logoImages/circleLogo.png';
+import Email from '../public/icons/Email.svg';
+import Map from '../public/icons/Map.svg';
 import { footerItems } from '@/lib/menuItems';
 
 export const Footer = () => {
@@ -24,43 +26,89 @@ export const Footer = () => {
     const sections = isMobile ? [...footerItems].reverse() : footerItems;
 
     return (
-        <footer className="pt-4 pb-6 bg-yellow rounded-t-xmd xl:rounded-t-xlg xl:px-24 xl:py-10">
-            <div className="container flex flex-col xl:flex-row xl:gap-60">
-                <Link
-                    href="/"
-                    className="flex items-center justify-center mb-8 xl:py-12 xl:mb-0"
-                    prefetch={false}
-                >
-                    <Image
-                        width={12}
-                        height={12}
-                        src={SquareLogo}
-                        quality={100}
-                        priority
-                        alt="Course logotype"
-                        className="mr-2"
-                    />
-                    <Image
-                        width={12}
-                        height={12}
-                        src={CircleLogo}
-                        quality={100}
-                        priority
-                        alt="Course logotype"
-                        className="mr-2"
-                    />
-                    <span className="">Logo</span>
-                </Link>
-
-                <nav className="flex flex-col gap-y-6 xl:flex-row xl:gap-x-20">
-                    {sections.map((section, index) => (
-                        <div key={index} className="flex flex-col">
-                            <h2 className="mb-6 px-6 font-normal text-lg text-footer_title xl:p-0">
-                                {section.title}
-                            </h2>
-                        </div>
-                    ))}
-                </nav>
+        <footer className="pt-100px bg-(--frame) rounded-t-[60px]">
+            <div className="container">
+                <div className="flex justify-between py-[40px]">
+                    <div>
+                        <Link
+                            href="/"
+                            prefetch={false}
+                            className="relative flex pl-[20px] mb-[32px]"
+                        >
+                            <Image
+                                width={12}
+                                height={12}
+                                src={SquareLogo}
+                                quality={100}
+                                priority
+                                alt="Course logotype"
+                                className="bottom-[40%] left-[0%] absolute"
+                            />
+                            <Image
+                                width={12}
+                                height={12}
+                                src={CircleLogo}
+                                quality={100}
+                                priority
+                                alt="Course logotype"
+                                className="bottom-[15%] left-[1%] absolute"
+                            />
+                            <span className="font-medium text-(--white) text-[20px]">Logo</span>
+                        </Link>
+                        <p className="mb-[94px] font-normal text-base text-(--white) w-[393.54px]">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatibus
+                            facere modi possimus dignissimos, aliquam nobis eaque? Voluptatem magnam
+                            quisquam rem.
+                        </p>
+                        <p className="font-normal text-base text-(--white)">
+                            &#169; 2024 Created by: Martin
+                        </p>
+                    </div>
+                    <div>
+                        <h3 className="mb-[20px] pt-[40px] font-semibold text-[20px] capitalize text-(--white)">
+                            Contact Information
+                        </h3>
+                        <ul className="mb-[66px] font-normal text-base text-(--white)">
+                            <li className="mb-[12px] flex">
+                                <Image
+                                    width={24}
+                                    height={24}
+                                    src={Email}
+                                    quality={100}
+                                    priority
+                                    alt="Email"
+                                    className="mr-[12px]"
+                                    style={{ filter: 'brightness(0) invert(1)' }}
+                                />
+                                example@support.com
+                            </li>
+                            <li className="mb-[66px] flex">
+                                <Image
+                                    width={24}
+                                    height={24}
+                                    src={Map}
+                                    quality={100}
+                                    priority
+                                    alt="Map"
+                                    className="mr-[12px]"
+                                    style={{ filter: 'brightness(0) invert(1)' }}
+                                />
+                                5 xyz st., adc, vallhalla
+                            </li>
+                        </ul>
+                        <nav>
+                            <ul className="flex gap-[32px] font-normal text-base text-[var(--white)]">
+                                {sections.map((section, index) => (
+                                    <li key={index}>
+                                        <Link href={section.href} prefetch={false}>
+                                            {section.title}
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
             </div>
         </footer>
     );
