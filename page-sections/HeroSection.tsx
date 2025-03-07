2dc7bc6d184e5d84c4cb55796554bb276655b23b
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-07 11:49:08 +0200

    add mobile styles

--- page-sections/HeroSection.tsx
+++ page-sections/HeroSection.tsx
@@ -8,32 +8,52 @@ import Avatar from '../public/heroImages/avatar.png';
 
 export const HeroSection = () => {
     return (
-        <section className={cn('hero relative top-[-44px] bg-cover bg-no-repeat bg-center')}>
-            <div className="absolute top-[110px] right-[8%] z-30">
-                <Image src={Brain} alt="Brain" width={494} height={494} />
+        <section
+            className={cn(
+                'hero relative rounded-b-[71px] xl:rounded-b-[72px] bg-cover bg-no-repeat bg-center'
+            )}
+        >
+            <div className="absolute top-[35%] right-[25%] xl:top-[13.5%] xl:right-[8%] z-1">
+                <Image
+                    src={Brain}
+                    alt="Brain"
+                    className="w-[192px] h-[203px] xl:w-[494px] xl:h-[494px] "
+                />
             </div>
 
-            <div className="absolute top-[110px] right-[20%]">
-                <Image src={Circle} alt="Circle" width={423} height={450} />
+            <div className="absolute top-[21%] right-[20%] xl:top-[110px] xl:right-[20%]">
+                <Image
+                    src={Circle}
+                    alt="Circle"
+                    className="w-[306px] h-[333px] xl:w-[423px] xl:h-[450px]"
+                />
             </div>
 
-            <div className="absolute bottom-[144px] right-[27%] z-40">
-                <Image src={Avatar} alt="Avatar" width={168} height={48} />
+            <div className="absolute bottom-[26%] right-[39%] xl:bottom-[21%] xl:right-[27%] z-2">
+                <Image
+                    src={Avatar}
+                    alt="Avatar"
+                    className="w-[88px] h-[25px] xl:w-[168px] xl:h-[48px]"
+                />
             </div>
-            <p className="absolute bottom-[143px] right-[8%] z-40 leading-[1.2] text-[16px] font-normal text-(--mainText) w-[205px]">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit!
-            </p>
 
-            <div className="pt-[200px] pl-[108px] ">
-                <h1 className="font-semibold text-5xl w-[511px] mb-[20px] text-(--white) leading-[1.2]">
+            <div className="mx-auto xl:mx-0 p-[62px_46px_51px_42px] xl:pt-[200px] xl:pl-[108px]">
+                <h1 className="text-center xl:text-left font-semibold text-[24px] xl:text-5xl w-[305px] xl:w-[511px] mb-[20px] text-(--white) leading-[1.2]">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit.
                 </h1>
-                <p className="leading-[1.2] mb-[80px] text-lg w-[511px] text-(--mainText) font-normal">
+                <p className="mx-auto xl:mx-0 text-center xl:text-left leading-[1.2] mb-[323px] xl:mb-[80px] text-sm xl:text-lg w-[285px] xl:w-[511px] text-(--mainText) font-normal">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. A officia molestiae
                     dolorum tempora ut accusamus cupiditate! Nesciunt tempora reiciendis libero
                     voluptate!
                 </p>
-                <Button variant="primary">Зарегистрироваться</Button>
+                <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-between">
+                    <Button variant="primary" className="order-2 md:order-1">
+                        Зарегистрироваться
+                    </Button>
+                    <p className="order-1 md:order-2 mb-[38px] xl:mb-0 text-center xl:text-left leading-[1.2] text-[10px] xl:text-[16px] font-normal text-(--mainText) w-[205px]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit!
+                    </p>
+                </div>
             </div>
         </section>
     );
