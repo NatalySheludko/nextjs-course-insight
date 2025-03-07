328da06256a1948e8bf60fe932489e9ac1a5a47d
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-07 18:06:36 +0200

    add mobile styles

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,10 +2,12 @@ import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <div className="mt-[-44px] p-80 bg-(--frame) flex flex-col justify-center items-center gap-y-4">
-            <h1 className="font-bold text-8xl xl:text-9xl text-(--backgroundLight)">404</h1>
+        <div className="container bg-(--frame) flex flex-col justify-center items-center gap-y-4">
+            <h1 className="pt-[250px] font-bold text-[80px] xl:text-8xl text-(--backgroundLight)">
+                404
+            </h1>
             <h2 className="text-3xl text-(--backgroundLight) font-normal">Page Not Found</h2>
-            <Link href="/" className="text-xl font-normal text-(--skyBlue)">
+            <Link href="/" className="pb-[250px] text-xl font-normal text-(--skyBlue)">
                 Go back home
             </Link>
         </div>
