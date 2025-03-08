82f136ec5ae39c80e57b3ea224124db9b13c1ba9
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2025-03-08 20:41:02 +0200

    fix button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -11,7 +11,7 @@ const buttonVariants = cva(
             variant: {
                 primary:
                     'font-semibold text-lg leading-[1.2] bg-white text-(--frame) border-none rounded-[40px] px-[32px] py-[16px] w-[232px] h-[54px] cursor-pointer',
-                filled: 'font-medium text-[8px] xl:text-xs xl:leading-[1.2] bg-(--frame) text-(--white) rounded-[28px] xl:rounded-[40px] px-[16px] py-[6px] w-auto cursor-pointer',
+                filled: 'font-medium w-[246px] text-base xl:text-lg xl:leading-[1.2] bg-(--frame) text-(--white) rounded-[40px] xl:rounded-[40px] px-[99px] py-[16px] xl:w-full cursor-pointer',
                 outlined:
                     'font-medium text-[10px] leading-[1.2] xl:leading-0 xl:text-xs border border-(--darkGray) w-auto text-(--darkGray) rounded-[28px] px-[16px] py-[6px] h-[24px] bg-transparent cursor-pointer',
                 outlinedDrop:
